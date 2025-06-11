aa6148d9b300bb0906c8876e902b8ad8c6362dac
Patrick Joyce <pjaudiomv@gmail.com>
2025-06-11 19:36:31 -0400

    fix spinner, rm modal

--- src/resources/js/components/SpinnerModal.svelte
+++ src/resources/js/components/SpinnerModal.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-  import { Modal, Spinner } from 'flowbite-svelte';
+  import { Spinner } from 'flowbite-svelte';
   import { spinner } from '../stores/spinner';
+  import { fade } from 'svelte/transition';
 
   let isActive = $derived(!!$spinner);
 </script>
 
-<Modal bind:open={isActive} color="none" class="shadow-none" size="xs" dismissable={false}>
-  <div class="text-center">
-    <Spinner />
+{#if isActive}
+  <div class="fixed inset-0 z-50 flex items-center justify-center bg-black/40" transition:fade={{ duration: 150 }}>
+    <Spinner size="10" />
   </div>
-</Modal>
+{/if}
