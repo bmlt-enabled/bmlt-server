0fb8672910655e961347fef8c4768c363c91f31f
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2025-06-12 17:30:59 -0400

    format things (#1217)

--- src/resources/js/routes/Formats.svelte
+++ src/resources/js/routes/Formats.svelte
@@ -97,13 +97,13 @@
   export function getFormatName(format: Format): string {
     const n = format.translations.find((t) => t.language === language);
     if (n) {
-      return n.name;
+      return `(${n.key}) ${n.name}`;
     } else {
       const e = format.translations.find((t) => t.language === 'en');
       if (e) {
-        return e.name + ' (' + $translations.noTranslationAvailable + ')';
+        return `(${e.key}) ${e.name} (${$translations.noTranslationAvailable})`;
       } else if (format.translations[0]) {
-        return format.translations[0].name + ' (' + $translations.noTranslationAvailable + ')';
+        return `(${format.translations[0].key}) ${format.translations[0].name} (${$translations.noTranslationAvailable})`;
       } else {
         return '';
       }
