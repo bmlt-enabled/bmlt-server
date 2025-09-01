16a675ae561aac8b8d819351dfae1ff20fdb2e24
Patrick Joyce <pjaudiomv@gmail.com>
2025-09-01 14:48:34 -0400

    error modal
    network error handling
    
    fix test

--- src/resources/js/stores/spinner.ts
+++ src/resources/js/stores/spinner.ts
@@ -4,5 +4,6 @@ const spinnerStore = writable(0);
 export const spinner = {
   subscribe: spinnerStore.subscribe,
   show: (): void => spinnerStore.update((n) => n + 1),
-  hide: (): void => spinnerStore.update((n) => n - 1)
+  hide: (): void => spinnerStore.update((n) => Math.max(0, n - 1)),
+  reset: (): void => spinnerStore.set(0)
 };
