215218fe13f44e184380f1aae8d2916eeec39366
Daniel Brotsky <dan@brotsky.com>
2025-10-31 18:10:23 -0700

    Fix color of chevron in BasicAccordion.
    The basic accordion has a dark background, and the open/close chevron was being drawn in the same color. This adjusts the color to be the same as the heading.

--- src/resources/js/components/BasicAccordion.svelte
+++ src/resources/js/components/BasicAccordion.svelte
@@ -25,11 +25,11 @@
     <span class="w-full">{header}</span>
 
     {#if open}
-      <svg class="h-3 w-3 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+      <svg class="h-3 w-3 text-gray-300 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5" />
       </svg>
     {:else}
-      <svg class="h-3 w-3 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+      <svg class="h-3 w-3 text-gray-300 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
       </svg>
     {/if}
