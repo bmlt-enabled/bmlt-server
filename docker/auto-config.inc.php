6b5f8d4bde8143b448f0888fce71eedbeca50717
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2026-02-01 15:43:54 -0500

    rm unused dbtype setting (#1459)

--- docker/auto-config.inc.php
+++ docker/auto-config.inc.php
@@ -1,5 +1,2 @@
 <?php
 defined( 'BMLT_EXEC' ) or die ( 'Cannot Execute Directly' );	// Makes sure that this file is in the correct context.
-
-// Database settings:
-$dbType = 'mysql'; // This is the PHP PDO driver name for your database.
